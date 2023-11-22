b8629717d7fba50f7085a5d952988ff26e3e328e
PeteJames21 <peetjames21@gmail.com>
2023-11-22 13:40:40 +0300

    Fix: Set default ID and timestamps if kwargs is not set

--- models/base_model.py
+++ models/base_model.py
@@ -17,11 +17,9 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         date_format = "%Y-%m-%dT%H:%M:%S.%f"
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.utcnow()
-        else:
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.utcnow()
+        if kwargs:
             for key, value in kwargs.items():
                 if key == "__class__":
                     continue
