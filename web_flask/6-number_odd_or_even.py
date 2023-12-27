fb0bcd9d973299df5c79777d4c7044bcb36e2156
PeteJames21 <peetjames21@gmail.com>
2023-12-27 12:45:37 +0300

    Add the /number_odd_or_even route

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""
+Hello world using Flask
+"""
+
+from flask import Flask
+from flask import render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def index():
+    """The root page"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """The /hbnb page"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c(text):
+    """The /c page"""
+    # Return "C " followed by the value of `text`
+    # Underscores are replaced with spaces
+    text = escape(text.replace("_", " "))
+    return f"C {text}"
+
+
+@app.route("/python")
+@app.route("/python/<text>")
+def python(text="is cool"):
+    """The /python page"""
+    # Return "Python " followed by the value of `text`
+    # Underscores are replaced with spaces
+    text = escape(text.replace("_", " "))
+    return f"Python {text}"
+
+
+@app.route("/number/<int:n>")
+def number(n):
+    """The /number page"""
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>")
+def number_template(n):
+    """The /number_template page"""
+    return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>")
+def number_odd_or_even(n):
+    return render_template("6-number_odd_or_even.html", n=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
