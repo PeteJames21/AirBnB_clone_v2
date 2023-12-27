73936eab91923b1698352a263745a453163d530f
PeteJames21 <peetjames21@gmail.com>
2023-12-27 12:06:08 +0300

    Add the /number route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+Hello world using Flask
+"""
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def index():
+    """The root page"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """The /hbnb page"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c(text):
+    """The /c page"""
+    # Return "C " followed by the value of `text`
+    # Underscores are replaced with spaces
+    text = escape(text.replace("_", " "))
+    return f"C {text}"
+
+
+@app.route("/python")
+@app.route("/python/<text>")
+def python(text="is cool"):
+    """The /python page"""
+    # Return "Python " followed by the value of `text`
+    # Underscores are replaced with spaces
+    text = escape(text.replace("_", " "))
+    return f"Python {text}"
+
+
+@app.route("/number/<int:n>")
+def number(n):
+    """The /number page"""
+    n = escape(n)
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
