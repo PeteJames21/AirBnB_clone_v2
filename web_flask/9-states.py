340871779c194b5327f7e6739578c6f759439d81
PeteJames21 <peetjames21@gmail.com>
2023-12-29 11:36:47 +0300

    Add the '/states' path

---
+++ web_flask/9-states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Run a Flask app that serves a list of cities in each state.
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown(error):
+    storage.close()
+
+
+@app.route("/states")
+@app.route("/states/<id>")
+def list_states(id=None):
+    """List all states in the database."""
+    states_dict = storage.all(State)
+    if not id:
+        states_list = list(storage.all(State).values())
+        return render_template("9-states.html", states=states_list)
+
+    state_obj = states_dict.get(f"State.{id}", None)
+    return render_template("9-states.html", state=state_obj)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
